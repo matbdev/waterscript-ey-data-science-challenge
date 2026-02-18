f20cb0b6cdde643088b4f0160745c3d09476ad3e
MATBDEV <mateuscarnielbrambilla@gmail.com>
2026-02-18 01:10:34 +0000

    fixes: renamed notebooks to follow styleguide added new features to benchmark notebook - now it groups rivers by lat+long and ensures that a river that appears on train df do not appear on test df (it floored r2, but i think is a good start)

---
+++ utils/functions.py
@@ -0,0 +1,16 @@
+def save_df(df, table_name, save_path):
+    '''
+    Saves a Pandas DataFrame locally as a CSV and uploads it to a Snowflake stage.
+    The file is saved temporarily to the /tmp/ directory before 
+    being uploaded via the Snowflake PUT command.
+    '''
+    df.to_csv(f'/tmp/{table_name}.csv', index = False)
+    
+    session.sql(f'''
+        PUT file:///tmp/{table_name}.csv
+        {save_path}
+        AUTO_COMPRESS=FALSE
+        OVERWRITE=TRUE
+    ''').collect()
+    
+    print(f'{table_name} saved! Refresh the browser to see the files in the sidebar')
