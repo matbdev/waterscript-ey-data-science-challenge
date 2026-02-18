5fc6a833c95901fd3fd2a8c718a99ea98ffd8d49
MATBDEV <mateuscarnielbrambilla@gmail.com>
2026-02-18 01:23:55 +0000

    fixed saving function and implementing it on landsat notebooks

--- utils/functions.py
+++ utils/functions.py
@@ -1,9 +1,13 @@
+import snowflake
+from snowflake.snowpark.context import get_active_session
+
 def save_df(df, table_name, save_path):
     '''
     Saves a Pandas DataFrame locally as a CSV and uploads it to a Snowflake stage.
     The file is saved temporarily to the /tmp/ directory before 
     being uploaded via the Snowflake PUT command.
     '''
+    session = get_active_session()
     df.to_csv(f'/tmp/{table_name}.csv', index = False)
     
     session.sql(f'''
